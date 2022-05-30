60f72485af278494e3d5a681a6dd97122c02de6f
TigranBalayan10 <tigranbalayan10@gmail.com>
2022-05-30 14:57:21 -0700

    request model setup

---
+++ server/models/Request.js
@@ -0,0 +1,39 @@
+const { Schema } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const requestSchema = new Schema(
+  {
+    moreInfo: {
+      type: String,
+      maxlength: 280,
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: ["Plumbing", "Electric", "Heating", "Carpentry", "Other"],
+      default: "Other",
+    },
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ["PENDING", "APPROVED", "REJECTED"],
+      default: "PENDING",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+  }
+);
+
+module.exports = requestSchema;
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,60 +1,59 @@
-const { User, Property, Apartment } = require('../models');
-const { AuthenticationError } = require('apollo-server-express');
-const { signToken } = require('../utils/auth');
+const { User, Property, Apartment } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
-      console.log('context', context.user);
+      console.log("context", context.user);
       if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id })
-          .populate('properties')
+        const userData = await User.findOne({ _id: context.user._id }).populate(
+          "properties"
+        );
 
         return userData;
       }
-      throw new AuthenticationError('You must be logged in to view this data');
-
+      throw new AuthenticationError("You must be logged in to view this data");
     },
 
     getProperties: async (parent, args, context) => {
       if (context.user) {
         console.log(context.user._id);
-        const match = context.user.roll === 'OWNER' ? { owner: context.user._id } : {};
-        const properties = await Property.find(match)
-        .populate(['owner'])
+        const match =
+          context.user.roll === "OWNER" ? { owner: context.user._id } : {};
+        const properties = await Property.find(match).populate(["owner"]);
         console.log(properties);
         return properties;
       }
 
-      throw new AuthenticationError('You must be logged in to view this data');
+      throw new AuthenticationError("You must be logged in to view this data");
     },
 
     getProperty: async (parent, { id }) => {
-      const property = await Property.findOne({ _id: id })
+      const property = await Property.findOne({ _id: id });
       return property;
     },
 
-    getApartments: async(parent, args, context) => {
-      const apartments = await Apartment.find({property: args.propertyId})
-      .populate(['tenant'])
+    getApartments: async (parent, args, context) => {
+      const apartments = await Apartment.find({
+        property: args.propertyId,
+      }).populate(["tenant"]);
       return apartments;
     },
 
     getUsers: async (parent, args) => {
-      const users = await User.find()
-        .select('-__v -password')
+      const users = await User.find().select("-__v -password");
       return users;
     },
 
     getUser: async (parent, { id }) => {
-      return User.findOne({ id })
-        .select('-__v -password')
-    }
+      return User.findOne({ id }).select("-__v -password");
+    },
   },
 
   Mutation: {
     addUser: async (parent, args) => {
-      console.log(await User.find())
+      console.log(await User.find());
       const user = await User.create(args);
       console.log(user);
       const token = signToken(user);
@@ -66,13 +65,13 @@ const resolvers = {
       const user = await User.findOne({ email, role });
 
       if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
 
       const token = signToken(user);
@@ -81,24 +80,27 @@ const resolvers = {
 
     addProperty: async (parent, args, context) => {
       console.log(context);
-      if (context.identity?.role === 'OWNER') {
-        const property = await Property.create({ ...args, owner: context.user._id });
+      if (context.identity?.role === "OWNER") {
+        const property = await Property.create({
+          ...args,
+          owner: context.user._id,
+        });
 
-        for(let i = 1; i <= args.numberOfApartments; i++) {
+        for (let i = 1; i <= args.numberOfApartments; i++) {
           await Apartment.create({
             number: i,
-            property: property._id
-          })
+            property: property._id,
+          });
         }
 
         return property;
       }
 
-      throw new AuthenticationError('You need to be logged in!');
+      throw new AuthenticationError("You need to be logged in!");
     },
 
     attachTenant: async (parent, { apartmentId }, context) => {
-      if (context.user && context.identity.role === 'TENANT') {
+      if (context.user && context.identity.role === "TENANT") {
         const apartment = await Apartment.findOneAndUpdate(
           { _id: apartmentId, tenant: null },
           { $set: { tenant: context.user._id } },
@@ -108,9 +110,9 @@ const resolvers = {
         return apartment;
       }
 
-      throw new AuthenticationError('You need to be logged in!');
+      throw new AuthenticationError("You need to be logged in!");
     },
-  }
+  },
 };
 
 module.exports = resolvers;
