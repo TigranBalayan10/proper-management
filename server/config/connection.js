0cdc5c7fde1c7ab79967afc6100f6a9cacc3711a
TigranBalayan10 <tigranbalayan10@gmail.com>
2022-05-31 18:04:49 -0700

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/management-db',
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/management-db",
   {
-    useNewUrlParser: true, 
+    useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
